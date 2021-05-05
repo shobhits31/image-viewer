cdbcfc71b74ee9f55cad446aef28d9f3239b604b
Shobhit Srivastava <shobhit3107@gmail.com>
2021-05-05 23:12:10 +0530

    Updated header component

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
 import "./Profile.css";
+import Header from "../../common/header/Header";
 
 class Profile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      accessToken: sessionStorage.getItem("access-token"),
+      loggedIn: sessionStorage.getItem("access-token") === null ? false : true,
+    };
+  }
+
   render() {
-    return <div>Profile Page</div>;
+    if (!this.state.loggedIn) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <div>
+        <Header loggedIn={this.state.loggedIn} history={this.props.history} />
+      </div>
+    );
   }
 }
 
