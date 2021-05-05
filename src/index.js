2915243b71059e8b903786ca6182ef9b8b7a80bb
Shobhit Srivastava <shobhit3107@gmail.com>
2021-05-05 20:49:51 +0530

    Strucutre for Header created

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import ImageViewerController from "./screens/ImageViewerController";
 
 ReactDOM.render(
-    <span>
-        Image Viewer
-    </span>, 
-    document.getElementById('root')
+  // <span>
+  //     Image Viewer
+  // </span>
+  <React.StrictMode>
+    <ImageViewerController />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
---
+++ src/screens/login/Login.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Header from "../../common/header/Header";
+
+class Login extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
